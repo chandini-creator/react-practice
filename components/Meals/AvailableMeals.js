65c780115f36804896e7bf31782864957b904c2c
chandini-creator <mchandini1307@gmail.com>
2023-03-13 12:09:30 +0530

    i

---
+++ components/Meals/AvailableMeals.js
@@ -0,0 +1,47 @@
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+
+const DUMMY_MEALS = [
+  {
+    id: 'm1',
+    name: 'Dosa',
+    
+    price: 22.99,
+  },
+  {
+    id: 'm2',
+    name: 'Biriyani',
+    
+    price: 16.5,
+  },
+  {
+    id: 'm3',
+    name: 'icecream',
+   
+    price: 12.99,
+  },
+  
+];
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
